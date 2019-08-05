2109f1a116d20f06edd2393ce0b7916d4210ff59
Gabriel Edefors <gabriel_edefors@hotmail.com>
2019-08-05 22:19:26 +0200

    Version 2

--- canvas.py
+++ canvas.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import numpy as np
 
 
 class Canvas(tk.Frame):
@@ -9,22 +10,26 @@ class Canvas(tk.Frame):
         # Create the canvas object
         self.scrollregion_dimension = 10000
         self.canvas = tk.Canvas(self, bg='#FFFFFF',
-                                scrollregion=(0, 0, 2 * self.scrollregion_dimension, self.scrollregion_dimension))
+                                scrollregion=(-self.scrollregion_dimension, -self.scrollregion_dimension / 2, self.scrollregion_dimension, self.scrollregion_dimension / 2))
 
         # Create scrollbars for canvas
         self.scrollbars()
         self.canvas.pack(side="left", expand=True, fill="both")
+
+        # Bind a zoom method
+        self.scale = 1
+        self.canvas.bind("<MouseWheel>", self.zoomer)
 
         # Create grid
         self.gridsize_w = 50
         self.gridsize_h = 50
         self.create_grid()
 
-        # Bind a zoom method
-        self.scale = 1
-        self.canvas.bind("<MouseWheel>", self.zoomer)
-
         # Monitor mouse click for node positions
+        self.coord_nodes = np.empty((0, 2), float)
+
+        # Set iteration flag and call monitor_mouseclick
+        self.monitor_mouseclick_flag = 0
         self.canvas.bind("<ButtonPress-1>", self.monitor_mouseclick)
 
     def scrollbars(self):
@@ -39,11 +44,13 @@ class Canvas(tk.Frame):
 
     def create_grid(self):
 
-        for i in range(0, 2 * self.scrollregion_dimension, self.gridsize_w):
-            self.canvas.create_line([(i, 0), (i, self.scrollregion_dimension)])
+        a = self.canvas.bbox("all")
+        for i in range(-self.scrollregion_dimension, self.scrollregion_dimension, self.gridsize_w):
+            self.canvas.create_line([(i, -int(-self.scrollregion_dimension / 2)),
+                                     (i, int(-self.scrollregion_dimension / 2))])
 
-        for i in range(0, self.scrollregion_dimension, self.gridsize_h):
-            self.canvas.create_line([(0, i), (2 * self.scrollregion_dimension, i)])
+        for i in range(int(-self.scrollregion_dimension / 2), int(self.scrollregion_dimension / 2), self.gridsize_h):
+            self.canvas.create_line([(-self.scrollregion_dimension, i), (self.scrollregion_dimension, i)])
 
     def monitor_mouseclick(self, event):
 
@@ -56,24 +63,44 @@ class Canvas(tk.Frame):
         y = event.y
 
         # Round to grid
-        x = self.gridsize_w * self.scale * round(x_canvas / self.gridsize_w * self.scale)
-        y = self.gridsize_h * self.scale * round(y_canvas / self.gridsize_h * self.scale)
+        x = self.gridsize_w * self.scale * round(x_canvas / (self.gridsize_w * self.scale))
+        y = self.gridsize_h * self.scale * round(y_canvas / (self.gridsize_h * self.scale))
 
-        # Draw a node when clicked
+        # Draw a node when a click occurs
         rad = self.scrollregion_dimension / 1000 * self.scale
         self.canvas.create_oval(x-rad, y-rad, x+rad, y+rad, fill="red")
 
+        # Save the coordinates
+        self.coord_nodes = np.append(self.coord_nodes, [[x, y]], axis=0)
+
+        # Draw a line between each other node
+        x1 = self.coord_nodes[self.monitor_mouseclick_flag-1, 0]
+        y1 = self.coord_nodes[self.monitor_mouseclick_flag-1, 1]
+        x2 = self.coord_nodes[self.monitor_mouseclick_flag, 0]
+        y2 = self.coord_nodes[self.monitor_mouseclick_flag, 1]
+        self.canvas.create_line(x1, y1, x2, y2, width=2.5, activefill="red")
+
+        # Save
+
+        # Update flag
+        self.monitor_mouseclick_flag += 1
+
     def zoomer(self,event):
+
         if event.delta > 0:
-            self.canvas.scale("all", event.x, event.y, 1.1, 1.1)
+            self.canvas.scale("all", 0, 0, 1.1, 1.1)
 
             # Track scaling
             self.scale *= 1.1
         elif event.delta < 0:
-            self.canvas.scale("all", event.x, event.y, 0.9, 0.9)
+            self.canvas.scale("all", 0, 0, 0.9, 0.9)
 
             # Track scaling
             self.scale *= 0.9
         self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+
+        self.coord_nodes[-1, :] *= self.scale
+
+
 
 
